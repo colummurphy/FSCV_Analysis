ba3f191e391fba78c5ebe566c9d97fc9b4e8295e
Jiwon Choi <Jiwon Choi@DESKTOP-31A7S6D>
2022-05-31 18:48:45 -0400

    Added readcell and conditional. Still need to add empty sheet code

--- general/manualtrialsel.m
+++ general/manualtrialsel.m
@@ -11,26 +11,69 @@ if exist(manlist)>0
     [~,sheetsav] = xlsfinfo(manlist); %find # sheets in worksheet
     sheet=find(contains(sheetsav,type));
     if ~isempty(sheet)
-       % xlsdata = xlsread(manlist,sheet);
-       xlsdata=readtable(manlist,'Sheet',sheet);    %read xls as table (table format)
+       xlsdata=readcell(manlist);    %read xls as table (table format)
+        channels={}
+        for i= 1:size(xlsdata,2)
+            if ischar(xlsdata{1,i})==0
+                break
+            end
+            if contains(xlsdata{1,i}, 'Ch')
+                channels=[channels, xlsdata(:,i)]
+            elseif contains(xlsdata{1,i}, 'ch')
+                channels=[channels, xlsdata(:,i)]
+            end
+        end
+    
+        for i= 1:size(channels,2)
+            if contains(channels{1,i}, '1')
+                channels{1,i}=1
+            elseif contains(channels{1,i}, '2')
+                channels{1,i}=2
+            elseif contains(channels{1,i}, '3')
+                channels{1,i}=3
+            elseif contains(channels{1,i}, '4')
+                channels{1,i}=4
+            end
+        end
+        final=str2double(string(channels(2:end,:)))-99;
+
+
        %NEED TO CONVERT TO NUMBERS FOR EACH CHANNEL NOW
        % chbadtrials=xlsdata-100;        % 101 first trial
-        chbadtrials=xlsdata-99;        %default, 100 first trial
-        %CONDITIONAL IF A CHANNEL IS EMPTY MAKE chbad trials=alltrials
+       % chbadtrials=[final(1,:); final(2:size(final,1),:)-99];        %default, 100 first trial
+       % CONDITIONAL IF A CHANNEL IS EMPTY MAKE chbad trials=alltrials
     end
 end
-goodtrials=alltrials;         %default, all trials goodtrials
-goodtrials={};
-for ich=1:4
-    %add 5/2019 all channels, not just #2
-if ~isempty(chbadtrials)        
-    %use manual selected trials
-    if size(chbadtrials,2)>1
-        goodtrials{ich}=alltrials(find(~ismember(alltrials,chbadtrials(:,ich))));
-    else
-        %only single ch in excel sheet
-        goodtrials{ich}=alltrials(find(~ismember(alltrials,chbadtrials)));
-    end        
+
+for ch=1:4 %added these loops but i dont think this will work unless we remove/change lines 28, and 29 since that sets goodtrials=alltrials
+    if sum(~isnan(final(:,ch)))==0 %finds entirely empty channels
+        chbadtrials(:,ch)=alltrials; %assigns them as bad trials
+        goodtrials{ch}=[];
+    else 
+        goodtrials{ch}=alltrials(~ismember(alltrials,final(:,ch))); %for the empty trials in channels with noted bad trials, filters out the good trials 
+    end
 end
-end
-end
+
+% badtrials=zeros(size(final,1),size(final,2));
+% for i = 1:size(final,1)
+%     for j = 1:size(final,2)
+%         if isnan(final(i,j))
+%             badtrials(i,j)=alltrials(i,j);
+%         end
+%     end
+% end
+
+% goodtrials=alltrials;         %default, all trials goodtrials
+% goodtrials={};
+% for ich=1:4
+%     %add 5/2019 all channels, not just #2
+%     if ~isempty(chbadtrials)        
+%         %use manual selected trials
+%         if size(chbadtrials,2)>1
+%             goodtrials{ich}=alltrials(find(~ismember(alltrials,chbadtrials(:,ich))));
+%         else
+%             %only single ch in excel sheet
+%             goodtrials{ich}=alltrials(find(~ismember(alltrials,chbadtrials)));
+%         end        
+%     end
+% end
