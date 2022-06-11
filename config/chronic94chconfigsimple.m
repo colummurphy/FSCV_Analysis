77b1acac358ef08ebb87aa7a4cd92d9afbfbae8f
hschwerdt <h.n.schwerdt@gmail.com>
2022-06-11 16:12:27 -0400

    Quickly plot raster plots and mean traces for sessions according to outcome conditions to look at ITI patterns using rasterscripts.m

--- config/chronic94chconfigsimple.m
+++ config/chronic94chconfigsimple.m
@@ -4,7 +4,7 @@ sessionnum=94;
 ncschannels={'pl1-p5','p1-p5','p2-p5','cl1-cl4','cl3-cl4','cl4-cl6','cl3-cl6',...
     'eyex','eyed','lickx','pulse'};   %chronic65/67
 
-letterdrive='Z:';   %COPYHERE
+letterdrive='Y:';   %COPYHERE
 fscvdir='patra_fscv'; %COPYHERE
 
 paths{1}=fullfile(letterdrive,'data_MIT',fscvdir,'patra_chronic94_06252018','1dr','cvtotxt',filesep); %COPYHERE
